ba2c3a33582dd9d834c0d538f02239698718f1e2
Ashvini Jangid <ashvinijangid@gmail.com>
2024-11-12 09:34:14 +0530

    First Push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,13 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     ssr: false,
+    target: 'static',
     nitro: {
         output: {
             publicDir: path.join(__dirname, 'docs')
         }
     },
     app: {
-        baseURL: '/app_policy/docs/'
+        baseURL: '/app_policy/'
     }
 })
