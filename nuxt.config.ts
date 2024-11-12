283cbbb073da8e7fc06eac75a7ff8361816c1dd1
Ashvini Jangid <ashvinijangid@gmail.com>
2024-11-12 08:56:16 +0530

    First Push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "node:path";
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     ssr: false,
+    nitro: {
+        output: {
+            publicDir: path.join(__dirname, 'build')
+        }
+    },
 })
